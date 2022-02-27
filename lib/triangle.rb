81248d0341b4f83231a08104b01128b44a886a19
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-02-27 18:17:08 -0500

    commit tree 8cf3564a7200e03927dab109fb7944de69701574

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require "pry"
 class Triangle
-  # write code here
+
+  attr_reader :s1, :s2, :s3
+
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind 
+    # binding.pry 
+    if (!self.valid)
+      raise TriangleError    
+    elsif @s1 == @s2 && @s2 == @s3
+      :equilateral
+    elsif (@s1 == @s2 || @s2 == @s3 || @s1==@s3)
+      :isosceles 
+    else 
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
+  private 
+
+  def valid
+    sides_above_zero = (@s1 > 0 && @s2 > 0 && @s3 > 0)
+    valid_ratio = (@s1+@s2>@s3 && @s2+@s3>@s1 && @s1+@s3>@s2)
+    sides_above_zero && valid_ratio
+  end
+
 end
